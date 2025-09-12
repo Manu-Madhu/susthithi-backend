ed9dd78b9a20486ee0fb2675fd6df87b989746af
Manu-Madhu <manum66466@gmail.com>
2025-09-12 16:30:19 +0530

    update the proxy part

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const bodyParser = require("body-parser");
 const logger = require("./utils/logger.js");
 
 const app = express();
+app.set("trust proxy", 1);
 
 app.use(helmet());
 app.use(cors());
