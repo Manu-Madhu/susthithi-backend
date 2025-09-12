6df35c1a8cbec182fbbcefc7fa2c8fdc03946e81
Manu-Madhu <manum66466@gmail.com>
2025-09-13 02:34:37 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,44 +1,54 @@
 require("dotenv").config();
 const cors = require("cors");
-const morgan = require("morgan");
 const express = require("express");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
+const cookieParser = require("cookie-parser");
+const { createHomePage } = require("./config/info.js");
 const applicationRoutes = require("./routes/application.routes.js");
 const errorHandler = require("./middlewares/errorHandler.midd.js");
-const { createHomePage } = require("./config/info.js");
 
 const bodyParser = require("body-parser");
 const logger = require("./utils/logger.js");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
-// app.set("trust proxy", 1);
 
-app.use(helmet());
-app.use(cors());
-app.use(morgan("combined"));
-app.use(
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
-    },
-  })
-);
+const { user_dev_url, admin_dev_url, user_prod_url, admin_prod_url, NODE_ENV } =
+  process.env;
+
+app.use(cookieParser());
+
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || NODE_ENV === "development") {
+      callback(null, true);
+    } else {
+      const allowedOrigins = [
+        user_dev_url,
+        admin_dev_url,
+        user_prod_url,
+        admin_prod_url,
+      ];
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Origin not allowed by CORS"));
+      }
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// const limiter = rateLimit({
-//   windowMs: 1 * 60 * 1000,
-//   max: 100,
-// });
-// app.use(limiter);
 
 // Routes
 app.use("/api", applicationRoutes);
 
-// Health
+// Health check
 app.get("/health", (req, res) => res.json({ ok: true }));
 
-// common page
+// Default page
 app.get("/", (req, res) => {
   const homePage = createHomePage({ name: "Susthini Server" });
   return res.send(homePage);
@@ -46,5 +56,14 @@ app.get("/", (req, res) => {
 
 // Error handler (must be last)
 app.use(errorHandler);
+
+process.on("unhandledRejection", (reason) => {
+  logger.error(`Unhandled Rejection: ${reason}`);
+});
+
+process.on("uncaughtException", (err) => {
+  logger.error(`Uncaught Exception: ${err.stack || err}`);
+  process.exit(1);
+});
 
 module.exports = app;
